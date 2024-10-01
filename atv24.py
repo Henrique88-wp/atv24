de1b62ace92be52c4fd429d1a4f2a66c679e9747
Henrique88-wp <phdsn411@gmail.com>
2024-10-01 12:50:27 -0300

    atv24 media

--- atv24.py
+++ atv24.py
@@ -1,15 +1,12 @@
 # Crie um programa que receba uma quantidade indefinida de números do usuário. O programa deve calcular a média desses números e parar quando o usuário digitar -1.
 
-soma = 0 
-cont = 0
+lista = []
+
 while True:
-    numero = int(input("coloque números"))
-    if numero == -1:
+    a = int(input("digite um número (caso queira terminar digite -1)"))
+    if a == -1:
         break
-    soma += numero
-    cont += 1
-if cont > 0:
-    media = soma/cont
-    print (f"a média dos números é: {media}")
-else:
-    print("eu quero números.")
+    lista.append(a)
+
+med = sum (lista) / len(lista)
+print(f" a média é de: {med}")
